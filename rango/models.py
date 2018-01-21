858b754c74010666805efc1a075c33905ebab32d
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-01-21 18:47:43 +0000

    adding models.py

--- rango/models.py
+++ rango/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name=models.CharField(max_lenght=128, unique=True)
-    def__str__(self):
-        return self.name
+     name=models.CharField(max_lenght=128, unique=True)
+     def__str__(self):
+            return self.name
 	
 class Page(models.Model):
-	category=models.ForeignKey(Category)
-	title= models.CharField(max_lenght=128)
-	url=models.URLField()
-	views=models.IntegerField(dafault=0)
-	def__str__(self):
-	    return self.title
+	 category=models.ForeignKey(Category)
+	 title= models.CharField(max_lenght=128)
+	 url=models.URLField()
+	 views=models.IntegerField(dafault=0)
+	 def__str__(self):
+	          return self.title
 	
--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
 from rango import views
 from django.conf import settings
-from django.config.urls.static import static 
+from django.conf.urls.static import static 
 urlpatterns = [
-     url(r'^$', views.index, name='index'),
-
-	 ] + static(setting.MEDIA_URL, document_root=setting.MEDIA_ROOT)
-
-	 
+     url(r'^$', views.index, name='index')
+	 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
