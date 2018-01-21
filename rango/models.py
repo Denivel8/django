b8fc5b2abc27747ee86e729688fb55522c6c3221
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-01-21 19:24:38 +0000

    fixing wrong config

--- rango/models.py
+++ rango/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-     name=models.CharField(max_lenght=128, unique=True)
-     def__str__(self):
+     name = models.CharField(max_length=128, unique=True)
+	 
+     def __str__(self):
             return self.name
 	
 class Page(models.Model):
 	 category=models.ForeignKey(Category)
-	 title= models.CharField(max_lenght=128)
+	 title= models.CharField(max_length=128)
 	 url=models.URLField()
-	 views=models.IntegerField(dafault=0)
-	 def__str__(self):
+	 views=models.IntegerField(default=0)
+	 
+	 def __str__(self):
 	          return self.title
 	
