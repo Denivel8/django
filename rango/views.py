f3ece857c1652474a8fec0ad8bca7ce8d1fa47ed
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-02-02 12:04:20 +0000

    test if the added cookies work

--- rango/views.py
+++ rango/views.py
@@ -23,7 +23,7 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-
+    request.session.set_test_cookie()
 
     response = render(request, 'rango/index.html', context=context_dict)
     return response
@@ -31,12 +31,20 @@ def index(request):
 
 def about(request):
    
-    context_dict = {}
-   
-    print(request.method)
-    print(request.user)
-    response = render(request, 'rango/about.html', context=context_dict)
-    return response
+     context_dict = {}
+     
+     print(request.method)
+     print(request.user)
+     if request.session.test_cookie_worked():
+           print("TEST COOKIE WORKED!")
+     request.session.delete_test_cookie()
+    
+     response = render(request, 'rango/about.html', context=context_dict)
+     return response   
+    
+
+
+    
 
 
 def show_category(request, category_name_slug):
