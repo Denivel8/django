008e7c6b652c2b8ce9377a240d1aa26b5f864994
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-02-11 14:01:59 +0000

    urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -20,12 +20,8 @@ urlpatterns = [
    url(r'^login/$', views.user_login, name='login'),
    url(r'^restricted/', views.restricted, name='restricted'),
    url(r'^logout/$', views.user_logout, name='logout'),
-   url(r'^accounts/', include('registration.backends.simple.urls')),
+
 	 ]
      
      
      
-     
-     
-     
- 
