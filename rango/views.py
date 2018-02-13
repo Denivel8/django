4a659ce77d0971399141cd81475fcfb1ba0008c7
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-02-13 16:20:00 +0000

    last fix of Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -20,29 +20,24 @@ from django.core.urlresolvers import reverse
 
 
 def index(request):
-   
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-    request.session.set_test_cookie()
+    
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session.get('visits')
+    context_dict['visits'] = request.session.get('visits', 0)
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
 
 def about(request):
-     visitor_cookie_handler(request)
-     context_dict = {}
-     context_dict = request.session.get('visits')
-     print(request.method)
-     print(request.user)
-     if request.session.test_cookie_worked():
-           print("TEST COOKIE WORKED!")
-     request.session.delete_test_cookie()
-    
-     response = render(request, 'rango/about.html', context=context_dict)
-     return response   
+    request.session.set_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict['visits'] = request.session.get('visits',0)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
     
 
 
